0f924f19847ce5bd712722170146a2717f6303f6
Alisa <alisa@jonnix.co>
2021-11-08 17:42:12 +0200

    created navbar component and logo icon

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.scss';
+ 
+const Home = () => {
+    return (
+    <div className="navbar">
+        <img src='./homer-simpson.png' alt="logo" height="35px" width="35px" className="logo"/>
+    </div>
+    )
+}
+
+export default Home;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.scss';
+import Navbar from '../../components/Navbar/Navbar';
+ 
+const Home = () => {
+    return (
+    <div className="wrapper">
+        <Navbar/>
+    </div>
+    )
+}
+
+export default Home;
