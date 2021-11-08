095880e1e5ceee33d573e43e674d9e4ee4866456
Alisa <alisa@jonnix.co>
2021-11-08 17:46:58 +0200

    added icons to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,6 +4,8 @@ import './Navbar.scss';
 const Home = () => {
     return (
     <div className="navbar">
+        <img src='./search.png' alt="logo" height="20px" width="20px"/>
+        <img src='./notification.png' alt="logo" height="20px" width="20px" className="notification"/>
         <img src='./homer-simpson.png' alt="logo" height="35px" width="35px" className="logo"/>
     </div>
     )
